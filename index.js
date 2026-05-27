454af688f8aee3f337afc95d782772c2b1e7a30e
steakhouse00-sys <steakhouse00@gmail.com>
2026-05-27 22:57:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const line = require("@line/bot-sdk");
 
+const app = express();
+
 const config = {
   channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET
+  channelSecret: process.env.CHANNEL_SECRET,
 };
-
-const app = express();
 
 const client = new line.Client(config);
 
@@ -21,17 +21,13 @@ app.post("/webhook", line.middleware(config), (req, res) => {
 });
 
 function handleEvent(event) {
-  if (event.type !== "message") {
-    return Promise.resolve(null);
-  }
-
-  if (event.message.type !== "text") {
+  if (event.type !== "message" || event.message.type !== "text") {
     return Promise.resolve(null);
   }
 
   return client.replyMessage(event.replyToken, {
     type: "text",
-    text: "你剛剛說：" + event.message.text
+    text: `你剛剛說：${event.message.text}`,
   });
 }
 
