f6d779d2c7c2ff43f5b84aabd4f993924c0d218a
steakhouse00-sys <steakhouse00@gmail.com>
2026-05-27 23:08:56 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const config = {
 
 const client = new line.Client(config);
 
+// 紀錄資料
+const counters = {};
+
 app.post("/webhook", line.middleware(config), (req, res) => {
-  Promise
-    .all(req.body.events.map(handleEvent))
+  Promise.all(req.body.events.map(handleEvent))
     .then((result) => res.json(result))
     .catch((err) => {
       console.error(err);
@@ -20,19 +22,50 @@ app.post("/webhook", line.middleware(config), (req, res) => {
     });
 });
 
-function handleEvent(event) {
-  if (event.type !== "message" || event.message.type !== "text") {
+async function handleEvent(event) {
+  if (event.type !== "message") {
     return Promise.resolve(null);
+  }
+
+  if (event.message.type !== "text") {
+    return Promise.resolve(null);
+  }
+
+  const text = event.message.text;
+
+  // 偵測 +1
+  if (text.includes("+1")) {
+
+    // 抓名稱
+    const match = text.match(/@(.+?)\s*\+1/);
+
+    if (match) {
+      const name = match[1];
+
+      if (!counters[name]) {
+        counters[name] = 0;
+      }
+
+      counters[name]++;
+
+      return client.replyMessage(event.replyToken, {
+        type: "text",
+        text:
+`📋 ${name} 統計
+
+目前人數：${counters[name]} 人`
+      });
+    }
   }
 
   return client.replyMessage(event.replyToken, {
     type: "text",
-    text: `你剛剛說：${event.message.text}`,
+    text: `你說的是：${text}`,
   });
 }
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+  console.log("Server running on " + port);
 });
